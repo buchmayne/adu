720d7749dadc4da7b74ca55fb3818269ada0f1f0
buchmayne <michaelmbuchman@gmail.com>
2023-02-01 14:04:21 -0800

    more project setup

---
+++ config.py
@@ -0,0 +1,34 @@
+from configparser import ConfigParser
+from sqlalchemy.engine import URL, create_engine
+
+
+def config(filename="database.ini", section="postgresql"):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(
+            "Section {0} not found in the {1} file".format(section, filename)
+        )
+
+    return db
+
+
+def get_connection():
+    db = config()
+    url_object = URL.create(
+        drivername="postgresql+psycopg2",
+        username=db["user"],
+        password=db["password"],
+        host=db["host"],
+        database=db["database"],
+    )
+    return create_engine(url_object)
