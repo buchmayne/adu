1c505f787f9eb14131b95c1a17c2b66093548aea
buchmayne <michaelmbuchman@gmail.com>
2023-02-06 13:27:43 -0800

    script downloads all available new construction building permits from portland maps api, and writes to table in db. Next step is to create script to do a daily/weekly pull to get new permits. Also need to add logic in python or sql to get at the actual ADUs from the new construction permits

---
+++ src/get_historic_new_construction_permits.py
@@ -0,0 +1,35 @@
+import os
+import requests
+from dotenv import load_dotenv
+import pandas as pd
+from config import get_connection
+
+load_dotenv()
+API_KEY = os.getenv("PORTLAND_MAPS_API_KEY")
+
+
+portland_maps_permits_url = "https://www.portlandmaps.com/api/permit/"
+
+query = {
+    "api_key": API_KEY,
+    "search_type_Id": 8,
+    "date_to": "02/06/2023",  # date of initial download
+    "download": 1,
+}
+
+
+if __name__ == "__main__":
+    response = requests.get(portland_maps_permits_url, params=query)
+    df = pd.DataFrame(response.json()["results"])
+
+    engine = get_connection()
+
+    # write permits to db
+    df.to_sql(
+        name="portland_new_construction_permits",
+        con=engine,
+        if_exists="append",
+        index=False,
+    )
+
+    print("Permits succesfully added to database")
